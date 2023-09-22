81bc17833b6ccedd1b4296431b58f7cd35bcfd79
PavanHegde15 <145775124+PavanHegde15@users.noreply.github.com>
2023-09-22 18:49:15 +0530

    Generate Pattern 021436587109121114131615181720 using constraint.
    This Constraint is to Generate 021436587109121114131615181720 pattern in SV.

---
+++ number_pattern.sv
@@ -0,0 +1,40 @@
+class parent;
+
+rand bit[11:0] y[$];
+
+constraint c1
+{
+
+y.size()==20;
+foreach(y[i])
+{
+
+    if(i==0)
+    y[i]==i;
+    
+else
+{
+if(i%2==0)
+y[i]==(i-1);
+else
+y[i]==(i+1);
+}
+}
+}
+
+endclass
+module top();
+parent p;
+initial
+begin
+    p=new();
+    assert(p.randomize());
+    foreach(p.y[i])
+    begin
+    $write("%0d",p.y[i]);
+    end
+    $display();
+
+end
+
+endmodule
