35b7f68d9112f16afd8f43a553bd566f87f8b4f7
PavanHegde15 <145775124+PavanHegde15@users.noreply.github.com>
2023-09-22 21:54:55 +0530

    Constraint to generate upper or lower triangle matrix.
    This code contains constraint to generate upper or lower triangle matrix. The type of matrix generated is controlled by "up" random variable. if up=0 it will generate lower triangle matrix else upper triangle matrix.

---
+++ upper_lower_triangle_4_4_matrix.sv
@@ -0,0 +1,41 @@
+class upper_traingle;
+
+rand bit [3:0]m[4][4];
+rand bit up;
+constraint upper_lower_triangle_matrix
+{
+    foreach(m[i,j])
+    {
+        if(!up)
+        {
+        if(i<j)
+          m[i][j]==0;
+        else
+        m[i][j]!=0;
+        }
+        else{
+        if(i>j)
+        m[i][j]==0;
+        else
+        m[i][j]!=0;
+        }
+    }
+}
+
+endclass
+
+module top();
+upper_traingle p;
+initial
+begin
+    p=new();
+    repeat(3)
+    begin
+    assert(p.randomize());
+    $display("up=%0d",p.up);
+    foreach(p.m[i])
+    $display("%0p",p.m[i]);
+    end
+end
+
+endmodule
